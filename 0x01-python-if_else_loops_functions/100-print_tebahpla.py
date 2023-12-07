e732c2727553a9a3efb6cf4e38504cc5d47613a2
emusenule <emusenjuleww@gmail.com>
2023-12-07 03:21:36 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(122, 96, -1):
-    if i % 2:
-        i = i-32
-        print("{:c}".format(i), end='')
+for i in range(25, -1, -1):
+    print(chr(ord('z') - i), end='')
+    print(chr(ord('A') + i), end='')
