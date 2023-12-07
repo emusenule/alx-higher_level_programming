442564efe3e1d86a04a83def215fc7aa45f6eb78
emusenule <emusenjuleww@gmail.com>
2023-12-07 03:56:28 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def remove_char_at(str, n):
-    if n < 0:
-        return str
+def remove_char_at(s, n):
+    if n < 0 or n >= len(s):
+        return s
     else:
-        str=str[0:n]+str[n+1:]
-        return str
+        s = s[:n] + s[n+1:]
+        return s
