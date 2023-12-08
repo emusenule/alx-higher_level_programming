7d0ddbf5d139424fd5651024dbf0eb7f2b9cf29b
emusenule <emusenjuleww@gmail.com>
2023-12-08 07:31:43 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -11,4 +11,14 @@ def add_arguments(argv):
 
 if __name__ == "__main__":
     sum_result = add_arguments(sys.argv[1:])
-    print(sum_result)
+print(sum_result)
+
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
+
