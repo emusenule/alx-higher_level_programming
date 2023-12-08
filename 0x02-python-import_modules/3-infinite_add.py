2eceb2c6346c32afb3a5a20c559dbcdc1223ddc7
emusenule <emusenjuleww@gmail.com>
2023-12-08 07:50:22 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
 
-import sys
-from decimal import Decimal
-
-def add_arguments(argv):
-    total = Decimal(0)
-    for arg in argv:
-        total += Decimal(arg)
-    return total
-
-if __name__ == "__main__":
-    sum_result = add_arguments(sys.argv[1:])
-print(sum_result)
-
 if __name__ == "__main__":
     """Print the addition of all arguments."""
     import sys
@@ -21,4 +8,3 @@ if __name__ == "__main__":
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
     print("{}".format(total))
-
