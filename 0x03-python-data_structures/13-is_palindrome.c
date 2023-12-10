159e3549849d5b39a7592bcfbf5d9d118e08ee28
emusenule <emusenjuleww@gmail.com>
2023-12-10 05:43:16 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,99 +1,53 @@
-#include <stdio.h>
+#include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-/* Definition of a singly linked list node */
-struct Node 
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: head of listint_t
+ * @n: int to add in listint_t list
+ * Return: address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t** head, const int n)
 {
-	int data;
-	struct Node* next;
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
-
-/* Function to check if a singly linked list is a palindrome */
-
-int is_palindrome(struct Node** head) 
+/**
+ * is_palindrome - identify if a syngle linked list is palindrome
+ * @head: head of listint_t
+ * Return: 1 if it is palindrome else 0
+ */
+int is_palindrome(listint_t **head)
 {
-	/* Check if the list is empty or has only one element */
-	if (*head == NULL || (*head)->next == NULL) 
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
+
+	if(*head == NULL || head2->next == NULL)
+		return (1);
+	while (head2 != NULL)
 	{
-		return 1; /* Empty list or single-element list is considered a palindrome
-    }
-
-    struct Node* slow = *head;
-    struct Node* fast = *head;
-    struct Node* prev = NULL;
-
-    // Find the middle of the list and reverse the first half
-    while (fast != NULL && fast->next != NULL) {
-        fast = fast->next->next;
-
-        struct Node* temp = slow;
-        slow = slow->next;
-        temp->next = prev;
-        prev = temp;
-    }
-
-    // If the list has odd number of elements, skip the middle node
-    if (fast != NULL) {
-        slow = slow->next;
-    }
-
-    // Compare the reversed first half with the second half
-    while (prev != NULL && slow != NULL) {
-        if (prev->data != slow->data) {
-            return 0; // Not a palindrome
-        }
-        prev = prev->next;
-        slow = slow->next;
-    }
-
-    return 1; // Palindrome
-}
-
-// Function to create a new node with the given data
-struct Node* new_node(int data) {
-    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
-    new_node->data = data;
-    new_node->next = NULL;
-    return new_node;
-}
-
-// Function to insert a new node at the beginning of the list
-void push(struct Node** head, int data) {
-    struct Node* new_node = new_node(data);
-    new_node->next = *head;
-    *head = new_node;
-}
-
-// Function to print the elements of the list
-void print_list(struct Node* head) {
-    struct Node* current = head;
-    while (current != NULL) {
-        printf("%d ", current->data);
-        current = current->next;
-    }
-    printf("\n");
-}
-
-// Test the is_palindrome function
-int main() {
-    struct Node* head = NULL;
-
-    // Create a palindrome list: 1 -> 2 -> 3 -> 2 -> 1
-    push(&head, 1);
-    push(&head, 2);
-    push(&head, 3);
-    push(&head, 2);
-    push(&head, 1);
-
-    printf("List: ");
-    print_list(head);
-
-    int result = is_palindrome(&head);
-    if (result == 1) {
-        printf("The list is a palindrome.\n");
-    } else {
-        printf("The list is not a palindrome.\n");
-    }
-
-    return 0;
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
+	}
+	aux2 = aux;
+	while (*head != NULL)
+	{
+		if *((*head)->n != aux2->n)
+		{
+			free_listint(aux);
+			return (0);
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
+	}
+	free_listint(aux);
+	return (1);
 }
