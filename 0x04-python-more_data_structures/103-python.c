5b5d8c86b9599b9d2ce8491bfcd0ee564c80ac4e
emusenule <emusenjuleww@gmail.com>
2023-12-12 03:57:11 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,52 +1,52 @@
 #include <Python.h>
-#include <stdio.h>
 
-void print_python_object(PyObject *p)
+/**
+ * print_python_bytes - Print information about Python bytes objects
+ * @p: PyObject pointer to the object
+ *
+ * Return: void
+ */
+void print_python_bytes(PyObject *p)
 {
-	if (PyList_Check(p))
+	Py_ssize_t i, size;
+	char *str;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_CheckExact(p))
 	{
-		Py_ssize_t size = PyList_Size(p);
-		Py_ssize_t i;
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	size = PyBytes_Size(p);
+	str = PyBytes_AsString(p);
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", str);
+	printf("  first %ld bytes:", size < 10 ? size + 1 : 10);
+	for (i = 0; i <= size && i < 10; i++)
+		printf(" %02x", (unsigned char)str[i]);
+	putchar('\n');
+}
 
-		printf("[*] Python list info\n");
-		printf("[*] Size of the Python List = %zd\n", size);
-		printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+/**
+ * print_python_list - Print information about Python list objects
+ * @p: PyObject pointer to the object
+ *
+ * Return: void
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t i, size;
+	PyObject *item;
 
-		for (i = 0; i < size; i++)
-		{
-			printf("Element %zd: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
-		}
-	} else if (PyBytes_Check(p))
+	size = PyList_Size(p);
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < size; i++)
 	{
-		Py_ssize_t size;
-		char *bytes_str;
-		int i;
-
-		printf("[.] bytes object info\n");
-
-		if (!PyBytes_Check(p))
-		{
-			printf("  [ERROR] Invalid Bytes object\n");
-			return;
-		}
-
-		size = PyBytes_Size(p);
-		bytes_str = PyBytes_AsString(p);
-
-		printf("  size: %zd\n", size);
-		printf("  trying string: %s\n", bytes_str);
-
-		printf("  first %zd bytes: ", (size < 10) ? size + 1 : 10);
-		for (i = 0; i < 10 && i <= size; i++)
-		{
-			printf("%02x", (unsigned char)bytes_str[i]);
-			if (i == 9 || i == size)
-				break;
-			printf(" ");
-		}
-		printf("\n");
-	} else 
-	{
-		printf("[ERROR] Unknown object type\n");
+		item = PyList_GET_ITEM(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+		if (PyBytes_Check(item))
+			print_python_bytes(item);
 	}
 }
