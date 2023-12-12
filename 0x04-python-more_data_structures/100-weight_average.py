8151432d29096bd9308ceda6561e589f5a201e57
emusenule <emusenjuleww@gmail.com>
2023-12-12 01:49:06 -0800

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    sum_product = 0
+    total_weight = 0
+
+    for score, weight in my_list:
+        sum_product += score * weight
+        total_weight += weight
+
+    weighted_average = sum_product / total_weight
+    return weighted_average
